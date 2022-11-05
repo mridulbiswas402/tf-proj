Problem of memory overflow fixed by making the routing step space efficient now it is O(n) previously it was O(n^2). This implementation is also time efficient. This implementation makes no depreciated API call. Problem in validation step fixed.

capsule-network/src/caps-em-layer_v1.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_probability as tfp
+from tensorflow_probability import distributions as tfd
 from scipy import random
 
 @tf.function
@@ -47,36 +48,12 @@ class Capsule(keras.layers.Layer):
 
     def build(self, input_shape):
 
-        batch_size = input_shape[0]
-        n=input_shape[1]
-        k=self.caps_n
-        d=self.caps_dim
-
         self.W = self.add_weight(name='W',
                     shape=[1, input_shape[1], self.caps_n, self.caps_dim, input_shape[-1]],
                     dtype=tf.float64,
                     initializer='glorot_uniform',
                     trainable=True)
         
-        #initialization step.
-        init_mu = random.rand(batch_size,k, d)*20 - 10
-        self.mu = init_mu #initializing mean.
-
-        init_sigma = np.zeros((k, d, d))
-        for i in range(k):
-            init_sigma[i] = np.eye(d)
-        sigma = init_sigma
-        sigma=tf.expand_dims(sigma,axis=0)
-        self.sigma=tf.tile(sigma,[batch_size,1,1,1]) # initializing cov matrix.
-
-        init_pi = np.ones(k)/k
-        pi = init_pi
-        pi=tf.expand_dims(pi,axis=0)
-        self.pi=tf.tile(pi,[batch_size,1])
-
-        R=np.zeros(shape=(n,k))
-        R=tf.expand_dims(R,axis=0)
-        self.R=tf.tile(R,[batch_size,1,1]) # coupling coefficient.
         
     def call(self, input_tensor):
         assert input_tensor.shape[2]==self.caps_dim
@@ -105,70 +82,56 @@ class Capsule(keras.layers.Layer):
         """ caps_predicted.shape = [1,caps_n(i-1),caps_n(i),caps_dim(i),1]"""
 
         """ dynamic routing """
-        """#initialization step.
-        init_mu = random.rand(batch_size,k, d)*20 - 10
-        mu = init_mu #initializing mean.
+        #initialization step.
+        
+        pi=np.ones([batch_size,k])/k
+        mu=np.random.rand(batch_size,k,d)
+        sigma=np.ones([batch_size,k,d])
+        R=np.zeros(shape=(batch_size,n,k))
 
-        init_sigma = np.zeros((k, d, d))
-        for i in range(k):
-            init_sigma[i] = np.eye(d)
-        sigma = init_sigma
-        sigma=tf.expand_dims(sigma,axis=0)
-        sigma=tf.tile(sigma,[batch_size,1,1,1]) # initializing cov matrix.
-
-        init_pi = np.ones(k)/k
-        pi = init_pi
-        pi=tf.expand_dims(pi,axis=0)
-        pi=tf.tile(pi,[batch_size,1])
-
-        R=np.zeros(shape=(n,k))
-        R=tf.expand_dims(R,axis=0)
-        R=tf.tile(R,[batch_size,1,1]) # coupling coefficient."""
-
-        pi=tf.Variable(self.pi,dtype=tf.float64)
-        mu=tf.Variable(self.mu,dtype=tf.float64)
-        sigma=tf.Variable(self.sigma,dtype=tf.float64)
-        R=tf.Variable(self.R,dtype=tf.float64)
-
-        #print(mu.shape,pi.shape,sigma.shape,R.shape)
-
-        N=np.zeros((batch_size,n))
-        N=tf.Variable(N,dtype=tf.float64)
+        pi=tf.convert_to_tensor(pi,dtype=tf.float64)
+        mu=tf.convert_to_tensor(mu,dtype=tf.float64)
+        sigma=tf.convert_to_tensor(sigma,dtype=tf.float64)
+        R=tf.convert_to_tensor(R,dtype=tf.float64)
 
         r=self.r
         while(r):
           r=r-1
-          # E-step.
-          for i in range(k):
-              #tileing mu & sigma for 1 pass computation
-              """ for processing the whole batch at once."""
-              mu_tmp=tf.expand_dims(mu[:,i,:,],axis=1)
-              mu_tmp=tf.tile(mu_tmp,[1,n,1])
+          """ E-step. """
+          
+          x_tmp=tf.expand_dims(input_tensor,axis=1) # x.shape==[b,n,d]
+          x_tmp=tf.tile(x_tmp,[1,k,1,1]) # x_tmp.shape==[b,k,n,d]
 
-              sig_tmp=tf.expand_dims(sigma[:,i,:,:,],axis=1)
-              sig_tmp=tf.tile(sig_tmp,[1,n,1,1])
+          mu_tmp=tf.expand_dims(mu,axis=2) # mu.shape==[b,k,d]
+          mu_tmp=tf.tile(mu_tmp,[1,1,n,1])   # mu_tmp.shape==[b,k,n,d]
 
-              
-              N = tfp.distributions.MultivariateNormalFullCovariance(loc=mu_tmp,
-                                                              covariance_matrix=sig_tmp).prob(input_tensor)
+          sig_tmp=tf.expand_dims(sigma,axis=2) # sigma.shape==[b,k,d]
+          sig_tmp=tf.tile(sig_tmp,[1,1,n,1])   # sig_tmp.shape == [b,k,n,d]
 
-              R[:,:,i].assign(tf.expand_dims(pi[:,i],axis=1)*N)
-          R.assign(R/tf.reduce_sum(R,axis=2, keepdims=True))
+          N = tfd.MultivariateNormalDiag(loc=mu_tmp,scale_diag=sig_tmp).prob(x_tmp)
+          N = pi[:,:,None]*N
+          N = N/tf.expand_dims(tf.reduce_sum(N,axis=1),axis=1)
+          R = tf.transpose(N,perm=[0,2,1])
 
-          # M-step
-          N_k=tf.reduce_sum(R,axis=1)
-          pi=N_k/n
-          mu=tf.matmul(tf.transpose(R,perm=[0,2,1]),input_tensor)
-          mu=mu/N_k[:,:,None]
+          """ M-step. """
+          
+          # updating pi.
+          N_k = tf.reduce_sum(R,axis=1)
+          pi = N_k/n
 
-          for i in range(k):
-              tmp=input_tensor-tf.expand_dims(mu[:,i,:],axis=1)
-              tmp=tf.expand_dims(tmp,axis=-1)
-              tmp_T=tf.transpose(tmp,perm=[0,1,3,2])
-              res=tf.matmul(tmp,tmp_T)
-              res=tf.multiply(tf.reshape(R[:,:,i],[batch_size,n,1,1]),res)
-              res=tf.reduce_sum(res,axis=1)/tf.reshape(N_k[:,i],[batch_size,1,1])
-              sigma[:,i].assign(res)
+          # updating mu.
+          mu = tf.matmul(tf.transpose(R,perm=[0,2,1]),input_tensor)
+          mu = mu/N_k[:,:,None]
+
+          # updating sigma.
+          mu_tmp=tf.expand_dims(mu,axis=2)
+          mu_tmp=tf.tile(mu_tmp,[1,1,n,1])
+          x_tmp=x_tmp-mu_tmp
+          x_tmp=tf.square(x_tmp)
+          R_T=tf.transpose(R,perm=[0,2,1])
+          x_tmp = tf.multiply(tf.reshape(R_T,[batch_size,k,n,1]),x_tmp)
+          sigma = tf.reduce_sum(x_tmp,axis=2)/tf.reshape(N_k,[batch_size,k,1])
+          sigma=tf.sqrt(sigma)
               
         weighted_prediction=tf.multiply(caps_predicted,tf.reshape(R,[batch_size,n,k,1,1]))
         weighted_sum = tf.reduce_sum(weighted_prediction, axis=1, keepdims=True)